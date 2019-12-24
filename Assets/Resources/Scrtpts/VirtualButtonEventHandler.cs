a25ed14753256bf51b12d25eba380e309b607dde
KeplerVK <1009637800@qq.com>
2019-12-24 14:19:08 +0800

    first commit

---
+++ Assets/Resources/Scrtpts/VirtualButtonEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    public Animator ani;
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        ani.SetBool("IsRun", true);
+        Debug.Log("run");
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        ani.SetBool("IsRun", false);
+        Debug.Log("wait");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
